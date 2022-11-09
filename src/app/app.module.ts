d812c78178779b58c2e7184429a07411d2857f5c
Desnecesauron <cesinhasampa63@gmail.com>
2022-11-09 17:07:32 -0300

    "All" commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/enums/ETheme.enum.ts
@@ -0,0 +1,7 @@
+export enum ETheme {
+  ICON_MOON = 'moon',
+  TEXT_MOON = 'Dark',
+
+  ICON_SUN = 'sun',
+  TEXT_SUN = 'Light',
+}
